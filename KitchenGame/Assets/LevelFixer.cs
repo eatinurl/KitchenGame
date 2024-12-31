2dec788269e45047751cd99ba16bfe6caf38534a
eatinurl <germethan@gmail.com>
2024-12-31 15:03:13 -0800

    secondacommita

--- KitchenGame/Assets/LevelFixer.cs
+++ KitchenGame/Assets/LevelFixer.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class LevelFixer : MonoBehaviour
 {
+    public Tilemap tilemap;
     // Start is called before the first frame update
     void Start()
     {
